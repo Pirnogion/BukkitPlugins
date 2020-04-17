7aa008f9aed8025f0f0b92469332476ff80ea43f
pirnogion <glebprositapow@gmail.com>
2020-04-18 02:59:08 +0500

    Temporary version. Fixed problem with a double dot in 'toString' function.

--- src/main/java/data/Account.java
+++ src/main/java/data/Account.java
@@ -1,6 +1,6 @@
 package data;
 
-import java.util.ArrayList;
+import java.util.Map;
 
 public class Account
 {
@@ -8,25 +8,9 @@ public class Account
     private String uuid;
     private String created_at;
     private String updated_at;
-    private ArrayList<String> balances;
+    private Map<Integer, Integer> balances;
 
     public Account() {}
-
-    public Account(int id, String uuid, String created_at, String updated_at, ArrayList<String> balances)
-    {
-        this.id = id;
-        this.uuid = uuid;
-        this.created_at = created_at;
-        this.updated_at = updated_at;
-        this.balances = balances;
-    }
-
-    public Account(int id, String uuid, ArrayList<String> balances)
-    {
-        this.id = id;
-        this.uuid = uuid;
-        this.balances = balances;
-    }
 
     public int getId()
     {
@@ -48,7 +32,7 @@ public class Account
         return updated_at;
     }
 
-    public ArrayList<String> getBalances()
+    public Map<Integer, Integer> getBalances()
     {
         return balances;
     }
@@ -57,16 +41,30 @@ public class Account
     public String toString() {
         StringBuilder builder = new StringBuilder();
 
-        builder.append("Account: id = ");  builder.append(id);
-        builder.append(", uuid = ");       builder.append(uuid);
-        builder.append(", created_at = "); builder.append(created_at);
-        builder.append(", updated_at = "); builder.append(updated_at);
+        builder.append("Account: id = ");
+        builder.append(id);
+        builder.append(", uuid = ");
+        builder.append(uuid);
+        builder.append(", created_at = ");
+        builder.append(created_at);
+        builder.append(", updated_at = ");
+        builder.append(updated_at);
         builder.append(". \nBalances: ");
 
-        if ( balances.isEmpty() ) builder.append("none. ");
-        else for (String balance: balances)
+        if (balances != null)
         {
-            builder.append("\n - "); builder.append(balance);
+            for (int currency_id : balances.keySet())
+            {
+                builder.append("\n - currency_id = ");
+                builder.append(currency_id);
+                builder.append(", amount = ");
+                builder.append(balances.get(currency_id));
+            }
+            builder.append(".");
+        }
+        else
+        {
+            builder.append("none. ");
         }
 
         return builder.toString();
